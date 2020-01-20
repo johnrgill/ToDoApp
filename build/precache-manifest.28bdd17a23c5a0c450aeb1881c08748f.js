202d20e89684458bf4e33bca7eff2acd2e57de4a
John R Gill <johngillplease@gmail.com>
2020-01-20 16:34:03 -0400

    gh pages branch

---
+++ build/precache-manifest.28bdd17a23c5a0c450aeb1881c08748f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ba03614733de3e00aa685e594ded9c",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "04b452cd39e3b51ced1a",
+    "url": "/toDoApp/static/css/main.c6df234c.chunk.css"
+  },
+  {
+    "revision": "60efa2b8fd89ba805232",
+    "url": "/toDoApp/static/js/2.b3046f3e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/toDoApp/static/js/2.b3046f3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04b452cd39e3b51ced1a",
+    "url": "/toDoApp/static/js/main.afce8197.chunk.js"
+  },
+  {
+    "revision": "d439ed8132391761ae67",
+    "url": "/toDoApp/static/js/runtime-main.a1de21b8.js"
+  }
+]);
