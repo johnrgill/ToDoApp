16381dd3535e8e3f8ebeedd49656791e6fcf1d86
John R Gill <johngillplease@gmail.com>
2020-01-20 10:36:10 -0400

    Updates

---
+++ precache-manifest.dfc217e106430168198574020f78530d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9f815d2f3bd4599f377ec9c78fad21",
+    "url": "/toDoApp/index.html"
+  },
+  {
+    "revision": "70330b7e56179e3cfafa",
+    "url": "/toDoApp/static/css/main.c63ea37b.chunk.css"
+  },
+  {
+    "revision": "60efa2b8fd89ba805232",
+    "url": "/toDoApp/static/js/2.b3046f3e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/toDoApp/static/js/2.b3046f3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70330b7e56179e3cfafa",
+    "url": "/toDoApp/static/js/main.792673a6.chunk.js"
+  },
+  {
+    "revision": "d439ed8132391761ae67",
+    "url": "/toDoApp/static/js/runtime-main.a1de21b8.js"
+  }
+]);
