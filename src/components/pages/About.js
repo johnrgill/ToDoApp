b9b9ad69d4ac828804061ae188de0dba098fe543
John R Gill <johngillplease@gmail.com>
2020-01-20 12:33:22 -0400

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <React.Fragment>
-            <h1>About</h1>
+            <h1>About1</h1>
             <p>This is the ToDoList App v1.0.0 from a React Crash Course</p>
             <p>Right now it grabs 10 dummy ToDos from a json file, but you can add your own.</p>
         </React.Fragment>
