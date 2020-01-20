6f36beaebe51c79164beafd72cd2878885b1d320
John R Gill <johngillplease@gmail.com>
2020-01-20 12:31:13 -0400

    Update About.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,6 +5,7 @@ const About = () => {
         <React.Fragment>
             <h1>About</h1>
             <p>This is the ToDoList App v1.0.0 from a React Crash Course</p>
+            <p>Right now it grabs 10 dummy ToDos from a json file, but you can add your own.</p>
         </React.Fragment>
     );
 }
